76aabde5d51603bc8c4d43bacd14245fcc45f29f
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-04-14 15:19:16 -0500

    Primera versión 1.0

---
+++ JacoboArcila-AirCombat/Assets/Scripts/Misil.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Destroy (this.gameObject, 5);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		this.transform.Translate (0.0f, 0.0f, 5.0f);
+	}
+
+
+	void OnCollisionEnter(Collision collision) {
+
+		Destroy (collision.collider.gameObject);
+
+		GameObject explosion = GameObject.Instantiate (Resources.Load ("Prefabs/Explosion") as GameObject);
+		explosion.transform.position = collision.collider.gameObject.transform.position;
+
+
+		Destroy (this.gameObject);
+	
+	}
+
+
+
+
+}
